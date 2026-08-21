6bccbc107d13fa89378faefc9728d753cbdda440
Lucas Piancó <lucaspianco133@gmail.com>
2026-08-21 10:37:03 -0300

    configura ambiente Docker e conexão com MySQL

---
+++ Back_end/config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = getenv('MYSQL_HOST');
+$dbname = getenv('MYSQL_DATABASE');
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Erro na conexão com o banco de dados.");
+}
+
+?>
--- Back_end/public/index.php
+++ Back_end/public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once './var/www/config/database.php';
+echo "Backend PHP funcionando!<br>";
+echo "Conexão com MySQL estabelecida!";
+
 echo "PHP esta funcionado";
 
 ?>
